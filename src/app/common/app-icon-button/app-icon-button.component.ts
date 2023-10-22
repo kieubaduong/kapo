57ae55748f3e88c27d8a5fbe3daff2603f458de0
Kieu Ba Duong <75083331+kieubaduong@users.noreply.github.com>
2023-10-23 00:14:18 +0700

    done login screen

---
+++ src/app/common/app-icon-button/app-icon-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, isDevMode } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-button',
+  templateUrl: './app-icon-button.component.html',
+  styleUrls: ['./app-icon-button.component.css']
+})
+export class AppIconButtonComponent {
+  @Input() iconUrl: string = '';
+  @Input() buttonText: string = 'Default Button Text';
+  @Input() backgroundColor: string = 'white';
+  @Input() borderColor: string = 'black';
+  @Input() borderRadius: number = 4;
+
+  logMessage(): void {
+    if (isDevMode())
+    console.log('Button clicked!'); // Log your desired message
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  logMessage(): void {
+    console.log('Button clicked!'); // Log your desired message
+  }
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return '';
+  }
+}
