e650e4f4a23811a1d6f6c78e7d9d850bde97e0ec
Kieu Ba Duong <20520990@gm.uit.edu.vn>
2023-11-15 00:07:45 +0700

    Done question button

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,5 +1,4 @@
-import { Component, HostListener, ViewChild } from '@angular/core';
-import { PointsTooltipComponent } from '../common/custom-tooltip/points-tooltip.component';
+import { Component, HostListener } from '@angular/core';
 import KapoFactory from '../../data/models/kapo.factory';
 import Kapo from '../../data/models/kapo';
 import Quiz from '../../data/models/quiz';
@@ -12,51 +11,31 @@ import TrueOrFalseQuiz from '../../data/models/true.or.false.quiz';
   styleUrls: ['./quiz.component.css'],
 })
 export class QuizComponent {
-  isSidebarOpen = true;
-  isSidebarAnimating = false;
-  items: Kapo[] = [];
-  kapoFactory = new KapoFactory();
+  // UI related variables
+  sidebarOpen = true;
+  sidebarAnimating = false;
+  greyContainerHeight: string = '52px';
+  placeholder = 'Enter your question here...';
+  displayCounter = false;
+
+  // Data related variables
+  kapoItems: Kapo[] = [];
   selectedKapo: Kapo;
+  kapoFactory = new KapoFactory();
   questionType = 'quiz';
-  countdown = 120;
-  question = '';
+  remainingCharacter = 120;
+  questionText = '';
+  initialWindowWidth: number = window.innerWidth;
 
   constructor() {
-    this.items = [
+    this.kapoItems = [
       this.kapoFactory.createQuestion('Quiz'),
       this.kapoFactory.createQuestion('TrueOrFalse'),
       this.kapoFactory.createQuestion('TrueOrFalse'),
       this.kapoFactory.createQuestion('Quiz'),
     ];
 
-    this.selectedKapo = this.items[0];
-  }
-
-  
-
-  selectQuiz(index: number) {
-    this.selectedKapo = this.items[index];
-  }
-
-  toggleSidebar(): void {
-    this.isSidebarOpen = !this.isSidebarOpen;
-    this.isSidebarAnimating = true;
-
-    setTimeout(() => {
-      this.isSidebarAnimating = false;
-    }, 200);
-  }
-
-  isInstanceOfQuiz(item: any): boolean {
-    return item instanceof Quiz;
-  }
-
-  isInstanceOfSlide(item: any): boolean {
-    return item instanceof Slide;
-  }
-
-  isInstanceOfTrueFalseQuiz(item: any): boolean {
-    return item instanceof TrueOrFalseQuiz;
+    this.selectedKapo = this.kapoItems[0];
   }
 
   getTypeName(item: any): string {
@@ -73,7 +52,70 @@ export class QuizComponent {
     return 'Invalid type';
   }
 
+  // Check if the item is an instance of a specific class
+  isInstanceOfQuiz(item: any): boolean {
+    return item instanceof Quiz;
+  }
+
+  isInstanceOfSlide(item: any): boolean {
+    return item instanceof Slide;
+  }
+
+  isInstanceOfTrueFalseQuiz(item: any): boolean {
+    return item instanceof TrueOrFalseQuiz;
+  }
+
+  // UI related functions
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    if (window.innerWidth < this.initialWindowWidth * 0.75) {
+      this.sidebarOpen = false;
+    }
+  }
+
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+    this.sidebarAnimating = true;
+
+    setTimeout(() => {
+      this.sidebarAnimating = false;
+      this.adjustTextareaHeight({
+        target: document.getElementById('question'),
+      });
+    }, 200);
+  }
+
+  selectQuiz(index: number) {
+    this.selectedKapo = this.kapoItems[index];
+  }
+
   updateCountdown() {
-    this.countdown = 120 - this.question.length;
+    this.remainingCharacter = 120 - this.questionText.length;
+  }
+
+  adjustTextareaHeight(event: any): void {
+    event.target.style.height = '52px';
+    this.greyContainerHeight = '57px';
+    if (event.target.scrollHeight > event.target.clientHeight) {
+      const temp = event.target.scrollHeight + 5;
+      this.greyContainerHeight = temp + 'px';
+      event.target.style.height = event.target.scrollHeight + 'px';
+    } else if (
+      event.target.scrollHeight < parseInt(event.target.style.height)
+    ) {
+      event.target.style.height = event.target.scrollHeight + 'px';
+      this.greyContainerHeight = event.target.scrollHeight + 5 + 'px';
+    }
+  }
+
+  preventEnter(event: any): void {
+    event.preventDefault();
+  }
+
+  blurOnQuestion(): void {
+    this.displayCounter = false;
+    if (this.questionText.length === 0) {
+      this.placeholder = 'Enter your question here...';
+    }
   }
 }
